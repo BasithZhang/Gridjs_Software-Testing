e02241e158687c0490da164a6993b675f09d2be4
Han <johnson920625@gmail.com>
2025-12-11 16:34:54 +0800

    feat: add more tests

--- tests/02_Dashboard/07_Examples/data_source/dynamic_data_import.spec.js
+++ tests/02_Dashboard/07_Examples/data_source/dynamic_data_import.spec.js
@@ -1,1 +1,20 @@
 import { test, expect } from "@playwright/test";
+
+const url = "http://localhost:3000/docs/examples/import-function";
+
+test.describe("Grab the title", async () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(url);
+        await expect(page).toHaveURL(url);
+    });
+
+    test("1. Grab the h1 title: JSON", async ({ page }) => {
+        const title = page.getByRole("heading", {
+            name: "Dynamic data import",
+            level: 1,
+        });
+        await expect(title).toBeVisible();
+
+        await expect(title).toHaveText("Dynamic data import");
+    });
+});
